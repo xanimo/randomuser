bc917cae95c38c7062eb268d2771688e803b5562
xanimo <dakoda@xanimo.net>
2018-02-19 14:14:48 -0800

    comment out function in userList

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -18,27 +18,27 @@ export class UserList extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  getRandomUsers(userCount, gender, minYear, maxYear) {
-  const promise = fetch(`https://randomuser.me/api/?results=${userCount}&nat=US&gender=${gender}`)
-    .then(response => {
-      if(response.status >= 400) {
-        throw `Response invalid ( ${response.status} )`;
-        return;
-      }
+//   getRandomUsers(userCount, gender, minYear, maxYear) {
+//   const promise = fetch(`https://randomuser.me/api/?results=${userCount}&nat=US&gender=${gender}`)
+//     .then(response => {
+//       if(response.status >= 400) {
+//         throw `Response invalid ( ${response.status} )`;
+//         return;
+//       }
 
-      return response.json();
-    })
-    .then(({results}) => {
-      return results;
-      console.log('results: ' + results);
-    })
-    .catch(error => {
-      console.log(error);
-    });
+//       return response.json();
+//     })
+//     .then(({results}) => {
+//       return results;
+//       console.log('results: ' + results);
+//     })
+//     .catch(error => {
+//       console.log(error);
+//     });
 
-  return promise;
-  console.log(promise);
-}
+//   return promise;
+//   console.log(promise);
+// }
 // const getNewsById = (state, id) => state.byId[id];
 
 // const getListByDate = (state, date) => state.listByDate[date];
